b6a06ba726a37c9fe4770dd469524c0391fc970f
Syed Ragib Israq <ragib.israq9833@gmail.com>
2023-03-16 14:25:52 +0600

    Complete

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -6,6 +6,10 @@ import { EarthCanvas } from './canvas';
 import { SectionWrapper } from '../hoc';
 import { slideIn } from '../utils/motion';
 
+//Service id- service_infvn8g
+//Public_key= WGVSxRlI-T-qY6OIu
+//template_id: template_apxhov6
+
 
 const Contact = () => {
   const formRef = useRef();
@@ -15,8 +19,44 @@ const Contact = () => {
     message: '',
   });
   const [loading, setLoading] = useState(false);
-  const handleChange = (e) => {}
-  const handleSubmit = (e) => {}
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setForm({ ...form, [name]: value })
+  }
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setLoading(true);
+
+    emailjs.send(
+      'service_infvn8g',
+      'template_apxhov6',
+
+      {
+        from_name: form.name,
+        to_name: 'Israq',
+        from_email: form.email,
+        to_email: 'syedisraq2403@gmail.com',
+        message: form.message,
+      },
+      'WGVSxRlI-T-qY6OIu'
+    )
+    .then(() => {
+      setLoading(false);
+      alert('Thank You. I will get back to you as soon as possible.');
+  
+
+    setForm({
+      name:'',
+      email:'',
+      message:'',
+    })
+  }, (error) => {
+    setLoading(false)
+    console.log(error);
+    alert('Something went wrong.')
+  })
+
+}
 
   return (
     <div className='xl:mt-12 xl:flex-row flex-col-reverse flex gap-10 overflow-hidden'>
@@ -36,48 +76,36 @@ const Contact = () => {
           className='mt-12 flex flex-col gap-8'
         >
           <label className='flex flex-col'>
-            <span className='text-white
-            font-medium mb-4'>Your Name</span>
-            <input 
-              type="text"
+            <span className='text-white font-medium mb-4'>Your Name</span>
+            <input
+              type='text'
               name='name'
               value={form.name}
               onChange={handleChange}
               placeholder="What's your good name?"
-              className='bg-tertiary py-4 px-6
-              placeholder:text-secondary
-              text-white rounded-lg outlined-none
-              border-none font-medium'
+              className='bg-tertiary py-4 px-6 placeholder:text-secondary text-white rounded-lg outline-none border-none font-medium'
             />
           </label>
           <label className='flex flex-col'>
-            <span className='text-white
-            font-medium mb-4'>Your message</span>
-            <textarea 
+            <span className='text-white font-medium mb-4'>Your email</span>
+            <input
+              type='email'
+              name='email'
+              value={form.email}
+              onChange={handleChange}
+              placeholder="What's your web address?"
+              className='bg-tertiary py-4 px-6 placeholder:text-secondary text-white rounded-lg outline-none border-none font-medium'
+            />
+          </label>
+          <label className='flex flex-col'>
+            <span className='text-white font-medium mb-4'>Your Message</span>
+            <textarea
               rows={7}
               name='message'
               value={form.message}
               onChange={handleChange}
-              placeholder="What do you want to say?"
-              className='bg-tertiary py-4 px-6
-              placeholder:text-secondary
-              text-white rounded-lg outlined-none
-              border-none font-medium'
-            />
-          </label>
-          <label className='flex flex-col'>
-            <span className='text-white
-            font-medium mb-4'>Your Name</span>
-            <input 
-              type="text"
-              name='name'
-              value={form.name}
-              onChange={handleChange}
-              placeholder="What's your good name?"
-              className='bg-tertiary py-4 px-6
-              placeholder:text-secondary
-              text-white rounded-lg outlined-none
-              border-none font-medium'
+              placeholder='What you want to say?'
+              className='bg-tertiary py-4 px-6 placeholder:text-secondary text-white rounded-lg outline-none border-none font-medium'
             />
           </label>
           <button
@@ -91,8 +119,15 @@ const Contact = () => {
           </button>  
         </form>
       </motion.div>
+      <motion.div
+        variants={slideIn('right', "tween", 0.2, 1)}
+        className="xl:flex-1 xl:h-auto md:h-[550px] h-[350px]"
+      >
+
+        <EarthCanvas/>
+      </motion.div>
     </div>
-  )
-}
+  );
+};
 
 export default SectionWrapper(Contact, "contact");
